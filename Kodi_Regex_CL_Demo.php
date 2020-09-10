9dec158c87a9c88408693d0941cab7155497522e
Albdroid.AL <sxt@usa.com>
2020-09-10 14:38:07 +0200

    Twitch TV V2 Muti Hosted Version

---
+++ Kodi_Regex_CL_Demo.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+  ┌────────────────────────────────────────────────────────────┐ 
+  |  For More Modules Or Updates Stay Connected to Kodi dot AL | 
+  └────────────────────────────────────────────────────────────┘ 
+  ┌───────────┬────────────────────────────────────────────────┐ 
+  │ Product   │ Twitch HLS Stream Extractor by Channel Name    │ 
+  │ Version   │ v2.0 PRO-DEV                                   │ 
+  │ Provider  │ https://www.twitch.tv                          │ 
+  │ Support   │ M3U8/VLC/KODI/SMART TV/Xream Codes Panel ETC   │ 
+  │ Licence   │ Personal                                       │ 
+  │ Author    │ Olsion Bakiaj                                  │ 
+  │ Email     │ TRC4@USA.COM                                   │ 
+  │ Author    │ Endrit Pano                                    │ 
+  │ Email     │ INFO@ALBDROID.AL                               │ 
+  │ Website   │ https://kodi.al                                │ 
+  │ Facebook  │ /albdroid.official/                            │ 
+  │ Created   │ Thursday, 02 January 2020                      │ 
+  │ Modified  │ Thursday, 10 September 2020                    │ 
+  └────────────────────────────────────────────────────────────┘
+*/
+
+// HOSTED VERSION APIS https://paidcodes.albdroid.al/Twitch_TV_Apis/
+
+error_reporting(0);
+set_time_limit(0);
+date_default_timezone_set("Europe/Tirane");
+
+/*
+ Generator @ Kodi dot AL Dev Tools
+ Code For PHP 5/7
+*/
+
+ob_start();
+$php_file = "Kodi_Regex.php";
+header("Location: https://paidcodes.albdroid.al/Twitch_TV_Apis/". $php_file);
+ob_end_flush();
+?>
